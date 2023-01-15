87d0e68bc4fe63457f6ed23247658f1db66f33cf
María José <58746507+mjmartinezstudium@users.noreply.github.com>
2023-01-15 22:31:18 +0100

    Add files via upload

---
+++ TallerED/src/holaMundo/HolaMundo.java
@@ -0,0 +1,12 @@
+package holaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// Imprimimos por consola un mensaje.
+		
+		System.out.println("¡Hola Mundo! Modifico el proyecto para subirlo a GitHub desde la consola de Git");
+		
+	}
+
+}
